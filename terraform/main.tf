da27586d3bcd5812a8c837a0d9bb86db8ae7770d
Alexwolf21 <163804464+Alexwolf21@users.noreply.github.com>
2025-03-03 09:03:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,20 +7,38 @@ terraform {
   }
 }
 
-provider "docker" {
-  host    = "npipe:////.//pipe//docker_engine"
+provider "docker" {}
+
+# Declare a Docker network
+resource "docker_network" "my_network" {
+  name = "my_network"
 }
 
-resource "docker_image" "nginx" {
+# Declare a Docker image for Nginx
+resource "docker_image" "nginx_image" {
   name         = "nginx:latest"
   keep_locally = false
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
+# Run an Nginx container attached to the network
+resource "docker_container" "nginx_container" {
+  name  = "nginx_server"
+  image = docker_image.nginx_image.name
+
+  # Attach to the network so that it can reach the sample-app container
+  networks_advanced {
+    name = docker_network.my_network.name
+  }
+
+  # Map container port 80 to host port 80
   ports {
     internal = 80
-    external = 8000
+    external = 80
+  }
+
+  # Mount our custom Nginx configuration
+  volumes {
+    host_path      = "C:/Users/AUROSHREE/Desktop/Project/terraform/nginx.conf"
+    container_path = "/etc/nginx/nginx.conf"
   }
 }
